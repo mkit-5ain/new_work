64ec2b616d43103a3972b032c069939dafcbf99d
mkit-5ain <mkit5ain@gmail.com>
2022-08-16 11:39:34 +0900

    [추가] component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,9 +3,23 @@
         <router-link to="/">Home</router-link> |
         <router-link to="/about">About</router-link>
     </nav>
-    <router-view/>
+    <ui-header/>
+    <article id="container" class="container">
+        <router-view/>
+    </article>
+    <ui-footer/>
 </template>
-
-<style>
-    @import '@/assets/css/common.css';
+<script>
+    import Header from '@/components/Header.vue';
+    import Footer from '@/components/Footer.vue';
+    export default {
+        name: 'App',
+        components: {
+            'ui-header': Header,
+            'ui-footer': Footer
+        }
+    }
+</script>
+<style lang="scss">
+    @import '@/assets/sass/common.scss';
 </style>
