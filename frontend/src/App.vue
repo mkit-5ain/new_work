510fb67585643d4968651c0a3c1049fb20ef59b4
mkit-5ain <mkit5ain@gmail.com>
2022-08-18 16:08:08 +0900

    [추가 및 삭제]

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,20 @@
 <template>
     <ui-header/>
-    <article class="container">
-        <router-view/>
-    </article>
+        <router-view class="container"/>
     <ui-footer/>
+    <curosr/>
 </template>
 <script>
     import LocomotiveScroll from 'locomotive-scroll';
     import Header from '@/components/Header.vue';
     import Footer from '@/components/Footer.vue';
+    import Cursor from '@/components/Cursor.vue';
     export default {
         name: 'App',
         components: {
             'ui-header': Header,
-            'ui-footer': Footer
+            'ui-footer': Footer,
+            'curosr': Cursor
         },
         mounted() {
             // const scroll = new LocomotiveScroll({
---
+++ frontend/src/components/Cursor.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="cursor"></div>
+</template>
+<script>
+export default {
+    name: 'Curosr',
+    mounted: function () {
+        this.cursorPointer(),
+        this.cursorActive();
+    },
+    methods: {
+        cursorPointer() {
+            const cursor = document.querySelector(".cursor");
+
+            document.addEventListener('mousemove', (e) => {
+                const mouseX = e.clientX;
+                const mouseY = e.clientY;
+                cursor.style.left = mouseX + 'px';
+                cursor.style.top = mouseY + 'px';
+                cursor.style.opacity = '1';
+            });
+
+        },
+        cursorActive() {
+            const cursor = document.querySelector(".cursor");
+            const cursorScale = document.querySelectorAll(".scale");
+
+            cursorScale.forEach(ele => ele.addEventListener("mouseover", () => {
+                cursor.style.transform = 'scale(0.5)';
+            }));
+
+            cursorScale.forEach(ele => ele.addEventListener("mouseout", () => {
+                cursor.style.transform = 'scale(1)';
+            }));
+
+            cursorScale.forEach(ele => ele.addEventListener("click", () => {
+                cursor.style.transform = 'scale(0.5)';
+            }));
+
+            cursorScale.forEach(ele => ele.addEventListener("mouseup", () => {
+                cursor.style.transform = 'scale(1)';
+            }));
+        }
+    }
+}
+</script>
