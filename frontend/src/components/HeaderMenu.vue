9969f6a37bd2c9a3a7b1a1af7c57af9a02bdaebb
mkit-5ain <mkit5ain@gmail.com>
2022-08-25 17:11:22 +0900

    [추가]

---
+++ frontend/src/components/HeaderMenu.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="menu-view">
+        <ul class="marquee-list left">
+            <li class="list-item"><a href="/">Home</a></li>
+            <li class="list-item"><a href="/">Home</a></li>
+            <li class="list-item"><a href="/">Home</a></li>
+            <li class="list-item"><a href="/">Home</a></li>
+        </ul>
+        <ul class="marquee-list right">
+            <li class="list-item"><a href="/Work">Work</a></li>
+            <li class="list-item"><a href="/Work">Work</a></li>
+            <li class="list-item"><a href="/Work">Work</a></li>
+            <li class="list-item"><a href="/Work">Work</a></li>
+        </ul>
+        <ul class="marquee-list left">
+            <li class="list-item"><a href="/Contact">Contact</a></li>
+            <li class="list-item"><a href="/Contact">Contact</a></li>
+            <li class="list-item"><a href="/Contact">Contact</a></li>
+            <li class="list-item"><a href="/Contact">Contact</a></li>
+        </ul>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'HeaderMenu'
+    }    
+</script>
+
+<style lang="scss" scoped>
+
+</style>
